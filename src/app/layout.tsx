1f80aae8f6ff83c48e744be74bf29a9c847e66fe
anderson-alves427 <oliveiraalves427@gmail.com>
2025-03-25 14:26:26 -0300

    refactor: layout and layout app

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { AntdRegistry } from "@ant-design/nextjs-registry";
+import Layout from "@/components/layout";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,7 +30,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <AntdRegistry>{children}</AntdRegistry>
+        <AntdRegistry>
+          <Layout>{children}</Layout>
+        </AntdRegistry>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,18 @@
 import AddNewDespesa from "@/components/addNewDespesa";
-import Layout from "@/components/layout";
 import ListDespesa from "@/components/listDespesa";
 import { despesasMock } from "@/mocks/despesasMock";
 import { Divider } from "antd";
 export default function Home() {
   return (
-    <Layout>
-      <div className="bg-white shadow-md rounded-lg py-3 px-2">
-        <div className="flex justify-between">
-          <p className="text-gray-400  font-semibold">
-            Total de despesas: <span className="text-gray-700">1555</span>
-          </p>
-          <AddNewDespesa />
-        </div>
-        <Divider />
-        <ListDespesa despesas={despesasMock} />
+    <div className="bg-white shadow-md rounded-lg py-3 px-2">
+      <div className="flex justify-between">
+        <p className="text-gray-400  font-semibold">
+          Total de despesas: <span className="text-gray-700">1555</span>
+        </p>
+        <AddNewDespesa />
       </div>
-    </Layout>
+      <Divider />
+      <ListDespesa despesas={despesasMock} />
+    </div>
   );
 }
