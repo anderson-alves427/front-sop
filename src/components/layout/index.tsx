ac3528d5b05e9569f4488f91046e9ffc99b60a7a
anderson-alves427 <oliveiraalves427@gmail.com>
2025-03-25 08:55:56 -0300

    style: create layout

---
+++ src/components/layout/index.tsx
@@ -0,0 +1,17 @@
+import { ReactNode } from "react";
+
+interface LayoutProps {
+  children: ReactNode;
+}
+
+export default function Layout({ children }: LayoutProps) {
+  return (
+    <div className="h-screen w-screen">
+      <header className="h-[10%] bg-sky-800 shadow-md flex justify-between items-center px-8 text-gray-100">
+        <h1 className="font-semibold">Sistema Sop</h1>
+        <div>Icone</div>
+      </header>
+      <div className="h-[90%]">{children}</div>
+    </div>
+  );
+}
